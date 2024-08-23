df0cf5ef4b27843817b274f53d1d6beb635fa65a
Bun <bun2932005@gmail.com>
2024-08-23 15:29:19 +0700

    update: title of user table

--- src/view/components/menuTable.js
+++ src/view/components/menuTable.js
@@ -2,15 +2,19 @@ const menuTable = () => {
 
    return `
       <div class="menu">
+         
+         <div class="menu-left">
+            <button class="menu-left-filter">
+               <img src="../assets/icons/filter.svg" alt="filter-icon">
+               <p>Filter</p>
+            </button>
 
-         <button class="menu-filter">
-            <p><img src="../assets/icons/filter.svg" alt="filter-icon">Filter</p>
-         </button>
-
-         <div class="menu-search">
-            <img src="../assets/icons/searchOption.svg" alt="search-bar">
-            <input type="text" placeholder="Search Users by Name, Email or Date" >
+            <div class="menu-left-search">
+               <img src="../assets/icons/searchOption.svg" alt="search-bar">
+               <input type="text" placeholder="Search Users by Name, Email or Date" >
+            </div>
          </div>
+
 
          <button class="menu-pay">PAY DUES</button>
       </div>

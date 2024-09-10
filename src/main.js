27c5a07182b1f1075f697b2eb7d3fa73d525f508
Bun <bun2932005@gmail.com>
2024-09-10 23:27:50 +0700

    rebuild: mvc

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import './main.scss';
 import './router/processRoutes';
-import filterController from './controllers/filterController';
-import View from './view/view';
 import processRoutes from './router/processRoutes';
+import FilterController from './controllers/filterController';
+import UserController from './controllers/userController';
+import View from './view/view';
 
-const controller = new filterController();
+const userController = new UserController();
 
-const view = new View(controller);
+const view = new View(userController);
 
+userController.view = view;
 processRoutes(view);
